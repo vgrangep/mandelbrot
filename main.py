0e7a38c8981f0d3887c5a1cf7ec8534a8cf3d76b
Vincent Grangé-Pradéras <vincent.grange@gmail.com>
2021-01-13 10:10:54 -0500

    change visualization to img instead of scatter, cleanup code and add functions

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
 import cmath
+from math import floor
 import numpy as np
 import matplotlib.pyplot as plt
+import tqdm
 
-def steps(c, itermax):
+
+def steps(c:complex, threshold:int = 2, itermax:int = 20):
     z = complex(0,0)
     j = 0
     result = False
-    while abs(z) < 2 and j < itermax:
+    while abs(z) < threshold and j < itermax:
         z = z ** 2 + c
         j += 1
-    if j == itermax:
-        result = True
-    return result
+    return j
 
 
-# Press the green button in the gutter to run the script.
+def generate_set(threshold, itermax, meshsize:int, x_min = -2, x_max = 0.48, y_min = -1.24, y_max = 1.24):
+    img = np.full((meshsize, meshsize), 255)
+
+    real = np.linspace(start = x_min, stop = x_max, num = meshsize)
+
+    for idr, r in zip(tqdm.trange(meshsize), real):
+        imaginary = zip(range(meshsize), np.linspace(start = y_min, stop = y_max, num = meshsize))
+        for idi, i in imaginary:
+            it = steps(complex(r, i), threshold = 2, itermax = itermax)
+            img[idi][idr] = 255 - floor(it * 2.5)
+    return img
+
+
 if __name__ == '__main__':
-    mesh = 10000
-    m = []
+    meshsize = 10_000
+    itermax = 100
+    threshold = 2
 
-    mandelbrot_x = []
-    mandelbrot_y = []
-    itermax = 20
+    x_min = -2.5
+    y_min = -2.5
+    x_max = 2.5
+    y_max = 2.5
 
-    real = np.linspace(start = -2, stop = 2, num = mesh)
-    for r in real:
-        img = np.linspace(start = -2, stop = 2, num = mesh)
-        for i in img:
-            c = complex(r,i)
-            if steps(c, itermax):
-                mandelbrot_x.append(r)
-                mandelbrot_y.append(i)
+    img = generate_set(threshold = 2, itermax = 25, meshsize = meshsize, x_max = x_max, x_min = x_min, y_min = y_min,
+                       y_max = y_max )
 
 
+    plt.figure(figsize=(10, 10))
+    plt.imshow(img, cmap="twilight_shifted")
 
-
-    plt.scatter(mandelbrot_x, mandelbrot_y, marker=".")
+    plt.axis("off")
     plt.show()
